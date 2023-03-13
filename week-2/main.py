809e08ccf437963106b21cb72b5e5df8deccd744
baranzeyn <baranzeyn676@gmail.com>
2023-03-13 11:45:55 +0300

    refactored

--- week-2/main.py
+++ week-2/main.py
@@ -23,16 +23,16 @@ fonksiyonları geliştiriniz ve her bir fonksiyonu en az bir kere çağırarak k
 """
 
 
-def exit():
+def exit1():
     """
 
     :return tüm listeyi bastırır ve sistemden çıkış yapar:
     """
-    print("****All Students****)
+    print("****All Students****")
     for i in names:
         print(i)
     print("logging out...")
-    exit(0)
+    exit(1)
 
 
 def learnSchoolNumber():
@@ -53,7 +53,7 @@ def learnSchoolNumber():
     if control(names, name):
         print(names.index(name))
     else:
-        print("student isn't exist")
+        print("student not exist")
 
 
 def register():
@@ -97,7 +97,7 @@ def delete():
             print("student deleted")
             print(names)
         else:
-            print("student doesn't exist")
+            print("student not exist")
 
     else:
         print("no registered students ")
@@ -110,24 +110,36 @@ def control(list, value):
     :param verileri içinde tutan liste:
     :param kontrol edilecek değişken:
     :return listede var olup olmaması:
+    burada böyle bir fonk yerine index methodu ile try except kullanarak da yapabiliriz.
+    # for i in list:
+    #     if i == value:
+    #         return True
+    #     else:
+    #         continue
+    # return False
+    index methodu aranılan değer varsa o değerin indexini verirken eğer yoksa hata veren bir methodtur. hata vermesi durumu için try-except kullandım.
+
     """
-    for i in list:
-        if i == value:
-            return True
-        else:
-            continue
-    return False
+    try:
+        index = list.index(value)
+    except:
+        return False
+    else:
+        return True
 
 
 names = []
 while True:
     choice = int(input("Please make a dicision:\n1-Register\n2-Remove\n3-Learn school number\n4-Exit\n:"))
-    match choice:
-        case 1:
-            register()
-        case 2:
-            delete()
-        case 3:
-            learnSchoolNumber()
-        case 4:
-            exit()
+    if choice > 0 and choice < 5:
+        match choice:
+            case 1:
+                register()
+            case 2:
+                delete()
+            case 3:
+                learnSchoolNumber()
+            case 4:
+                exit1()
+    else:
+        print("Something went wrong...\nTry again")
